679d1ee123eba2d0ae2c3f5625fe922d0a642782
wipes <wipes.6098@gmail.com>
2025-11-07 00:25:42 +0200

    jkymiu

--- app/src/main/java/soraka/ash/eliasfinalproject/MainActivity.java
+++ app/src/main/java/soraka/ash/eliasfinalproject/MainActivity.java
@@ -1,6 +1,9 @@
 package soraka.ash.eliasfinalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,55 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnAcc;
+    private Button btnGoals;
+    private Button btnAI;
+    private Button btnStatistics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        btnAcc =findViewById(R.id.btnAcc);
+        btnGoals =findViewById(R.id.btnGoals);
+        btnAI =findViewById(R.id.btnAI);
+        btnStatistics =findViewById(R.id.btnStatistics);
+        btnAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent =new Intent(MainActivity.this, accountsAndPay.class);
+                startActivity(intent) ;
+            }}
+        );
+        btnGoals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent =new Intent(MainActivity.this, goalsAbudgeting.class);
+                startActivity(intent) ;
+            }}
+        );
+        btnAI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent =new Intent(MainActivity.this, AIinsights.class);
+                startActivity(intent) ;
+            }}
+        );
+        btnStatistics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent =new Intent(MainActivity.this, statistics.class);
+                startActivity(intent) ;
+            }}
+        );
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);

42f4ea69647cfda9dbdbda0f1bf7d10071e77dcf
class01 <sorakaashwah14@gmail.com>
2025-11-06 14:09:30 +0200

    y64hr

--- app/src/main/java/soraka/ash/eliasfinalproject/Login.java
+++ app/src/main/java/soraka/ash/eliasfinalproject/Login.java
@@ -1,6 +1,8 @@
 package soraka.ash.eliasfinalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Login extends AppCompatActivity {
+    private Button bt_login;
+    private Button btnSignup;
+    private Button btnGoogle;
+    private Button btnFacebook;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +25,15 @@ public class Login extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        bt_login = findViewById(R.id.bt_login);
+        btnSignup = findViewById(R.id.btnSignup);
+        //btnGoogle = findViewById(R.id.btnGoogle);
+
+        // btnFacebook = findViewById(R.id.btnFacebook);
+        btnSignup.setOnClickListener(v -> {
+            Intent intent = new Intent(Login.this, signUp.class);
+            startActivity(intent);
         });
     }
 }
--- app/src/main/java/soraka/ash/eliasfinalproject/signUp.java
+++ app/src/main/java/soraka/ash/eliasfinalproject/signUp.java
@@ -1,6 +1,9 @@
 package soraka.ash.eliasfinalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class signUp extends AppCompatActivity {
+    private TextView titleText;
+    private TextView subtitleText;
+    private Button btnLogin;
+    private Button signUpButton;
+    private Button btnGoogle;
+    private Button btnFacebook;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,17 @@ public class signUp extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        titleText = findViewById(R.id.titleText);
+        subtitleText = findViewById(R.id.subtitleText);
+        btnLogin = findViewById(R.id.btnLogin);
+
+       // btnGoogle = findViewById(R.id.btnGoogle);
+       // btnFacebook = findViewById(R.id.btnFacebook);
+        signUpButton = findViewById(R.id.signUpButton);
+        btnLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(signUp.this, Login.class);
+            startActivity(intent);
         });
     }
 }

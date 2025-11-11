adbb3247d02db78a415cf5ee180cc50c7043c5ce
class01 <sorakaashwah14@gmail.com>
2025-11-11 10:46:27 +0200

    y64hr

---
+++ app/src/main/java/soraka/ash/eliasfinalproject/data/MyUserTable/MyProfile.java
@@ -0,0 +1,16 @@
+package soraka.ash.eliasfinalproject.data.MyUserTable;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+@Entity
+public class MyProfile {
+    @PrimaryKey(autoGenerate = true)//تحديد الصفة كمفتاح رئيسي والذي يُنتجح بشكل تلقائي
+    public long keyid;
+    @ColumnInfo(name = "full_Name")//اعطاء اسم جديد للعامود-الصفة في الجدول
+    public String fullName;
+    public String email;//بحالة لم يتم اعطاء اسم للعامود يكون اسم الصفه هو اسم العامود
+    public String phone;
+    public String passw;
+
+}

8d5e4bd36fdfeca045005366338419903405b56f
class01 <sorakaashwah14@gmail.com>
2025-11-11 13:24:45 +0200

    y64hr

--- app/src/main/java/soraka/ash/eliasfinalproject/MainActivity.java
+++ app/src/main/java/soraka/ash/eliasfinalproject/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnAI;
     private Button btnStatistics;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

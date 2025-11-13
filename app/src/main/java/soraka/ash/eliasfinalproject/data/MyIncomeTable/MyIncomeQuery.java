e3be6b572bd22948d400bb0c85b0edd0140f41cc
class01 <sorakaashwah14@gmail.com>
2025-11-13 14:09:42 +0200

    y64hr

--- app/src/main/java/soraka/ash/eliasfinalproject/data/MyIncomeTable/MyIncomeQuery.java
+++ app/src/main/java/soraka/ash/eliasfinalproject/data/MyIncomeTable/MyIncomeQuery.java
@@ -1,29 +1,55 @@
 package soraka.ash.eliasfinalproject.data.MyIncomeTable;
 
+import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
 
 import java.util.List;
-
+@Dao
 public interface MyIncomeQuery
 {
-    @Query("SELECT * FROM MyIncome")
-    List<MyIncome> getAll();
-    @Query("SELECT * FROM MyIncome WHERE transactionId IN (:userIds)")
-    List<MyIncome> loadAllByIds(int[] userIds);
-    @Query("SELECT * FROM MyIncome WHERE transactionId = :transactionId LIMIT 1")
-    MyIncome loadById(int transactionId);
     @Insert
-    void insertAll(MyIncome... users);
-    @Delete
-    void delete(MyIncome user);
+    void insert(MyIncome income);
     @Update
-    void update(MyIncome... values);
-    @Query("Delete From MyIncome WHERE transactionId=:id ")
-    void delete(int id);
-    @Query("Delete From MyIncome")
-    void deleteAll();
+    void update(MyIncome income);
+    @Delete
+    void delete(MyIncome income);
+
+    //get all incomes
+    @Query("SELECT * FROM MyIncome")
+    List<MyIncome> getAllIncomes();
+
+    //get income by id
+    @Query("SELECT * FROM MyIncome WHERE id = :incomeId LIMIT 1")
+    MyIncome getIncomeById(int incomeId);
+
+    //get incomes by user id
+    @Query("SELECT * FROM MyIncome WHERE userId = :userId")
+    List<MyIncome> getIncomesByUserId(int userId);
+
+    //get incomes by category id
+    @Query("SELECT * FROM MyIncome WHERE categoryId = :categoryId")
+    List<MyIncome> getIncomesByCategoryId(int categoryId);
+
+    //get completed incomes by user id
+    @Query("SELECT * FROM MyIncome WHERE userId = :userId AND isCompleted = 1")
+    List<MyIncome> getCompletedIncomesByUserId(int userId);
+
+    //get non completed incomes by user id
+    @Query("SELECT * FROM MyIncome WHERE userId = :userId AND isCompleted = 0")
+    List<MyIncome> getNonCompletedIncomesByUserId(int userId);
+
+
+
+
+
+
+
+
+
+
+
 
 }

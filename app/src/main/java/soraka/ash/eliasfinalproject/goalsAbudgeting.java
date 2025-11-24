ef030097274a0606d1244e04f6ea88f3719d2549
class01 <sorakaashwah14@gmail.com>
2025-11-24 08:48:49 +0200

    y64hr77

--- app/src/main/java/soraka/ash/eliasfinalproject/goalsAbudgeting.java
+++ app/src/main/java/soraka/ash/eliasfinalproject/goalsAbudgeting.java
@@ -2,23 +2,18 @@ package soraka.ash.eliasfinalproject;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import soraka.ash.eliasfinalproject.adapter.BudgetAdapter;
+import soraka.ash.eliasfinalproject.model.BudgetItem;
 
 public class goalsAbudgeting extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_goals_abudgeting);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
-}
